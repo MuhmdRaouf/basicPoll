760361811fef2c576bbd40bb95f3e697809d974f
Mohammed Abd El-Ra'ouf <mohammed.raouf.m@gmail.com>
2018-10-25 13:53:10 +0200

    finishing part 1 ' writing your first django app' tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_view(request):
+    return HttpResponse("Hello, World from django getting started!")
