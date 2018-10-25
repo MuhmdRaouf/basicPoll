33b52aa1702321bb2e106e5d7a77dfa3330bc3b7
Mohammed Abd El-Ra'ouf <mohammed.raouf.m@gmail.com>
2018-10-26 00:27:02 +0200

    finishing part 3 'writing your first django app' tutorial

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index_view
 
+from .views import index_view, detail_view, result_view, vote_view
+
+app_name = 'polls'
 urlpatterns = [
-    path("", index_view, name="HelloWorld")
+    path('', index_view, name='polls'),
+    path('<int:question_id>/', detail_view, name='detail'),
+    path('<int:question_id>/result/', result_view, name='result'),
+    path('<int:question_id>/vote/', vote_view, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,26 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Question
 
 
 # Create your views here.
 def index_view(request):
-    return HttpResponse("Hello, World from django getting started!")
+    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions_list': latest_questions_list}
+    return render(request, 'index.html', context)
+
+
+def detail_view(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'details.html', context)
+
+
+def result_view(request, question_id):
+    context = {'details': str(question_id)}
+    return render(request, 'result.html', context)
+
+
+def vote_view(request, question_id):
+    context = {'details': str(question_id)}
+    return render(request, 'vote.html', context)
